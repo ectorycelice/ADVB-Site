a51216cb1fec25009a8852777b1e2956750e63fd
ectorycelice <ectorycelice@gmail.com>
2026-04-16 12:41:24 -0300

    Initial commit

---
+++ final 2/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'advb-v1';
+const ASSETS = [
+  '/login.html',
+  '/index.html',
+  '/admin.html',
+  '/icon.jpg',
+  '/manifest.json'
+];
+
+// Instalar e cachear arquivos principais
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Limpar caches antigos
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Responder com cache quando offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
